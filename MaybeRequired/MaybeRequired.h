cfc10e9caafbde7305fa09af0293b38244b9d394
Mathew Polzin <matt.polzin@gmail.com>
2017-07-08 09:52:09 -0700

    xcode project added

---
+++ MaybeRequired/MaybeRequired.h
@@ -0,0 +1,19 @@
+//
+//  MaybeRequired.h
+//  MaybeRequired
+//
+//  Created by Mathew Polzin on 7/8/17.
+//  Copyright © 2017 Mathew Polzin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaybeRequired.
+FOUNDATION_EXPORT double MaybeRequiredVersionNumber;
+
+//! Project version string for MaybeRequired.
+FOUNDATION_EXPORT const unsigned char MaybeRequiredVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaybeRequired/PublicHeader.h>
+
+
